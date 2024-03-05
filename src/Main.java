e6e10ed1e6658bd15ac0f165418428784d29ccce
MohammedRamiz6216 <sq1941@srmist.edu.in>
2024-03-05 09:58:24 +0530

    add files

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,30 @@
+/*  int n= sc.nextInt();
+        int temp=0,last=0,digit=0,sum=0;
+        temp=n;
+        while(temp!=0){
+        temp=temp/10;
+        digit++;
+        }
+        temp=n;
+        while(temp!=0){
+        last=temp%10;
+        sum+=(Math.pow(last,digit));
+        temp=temp/10;
+        }
+        if(sum==n){
+        System.out.println("Is an armstrong number");
+        }
+        else{
+        System.out.println("Not an armstrong number");
+        }
+
+ */
+
+
 import java.util.*;
-
-
 public class Main {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
-       int n= sc.nextInt();
-       int temp=0,last=0,digit=0,sum=0;
-       temp=n;
-       while(temp!=0){
-           temp=temp/10;
-           digit++;
-       }
-       temp=n;
-       while(temp!=0){
-           last=temp%10;
-           sum+=(Math.pow(last,digit));
-           temp=temp/10;
-        }
-       if(sum==n){
-           System.out.println("Is an armstrong number");
-       }
-       else{
-           System.out.println("Not an armstrong number");
-       }
+
     }
 }
